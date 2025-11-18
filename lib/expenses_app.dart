e64156fdca9dd354d57924416a8b0bd40ee79d70
Laboratory 07 <lab07@lab.mu.edu>
2025-11-18 11:28:42 +0200

    first commit

---
+++ lib/expenses_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ExpensesApp extends StatefulWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  State<ExpensesApp> createState() => _ExpensesAppState();
+}
+
+class _ExpensesAppState extends State<ExpensesApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
