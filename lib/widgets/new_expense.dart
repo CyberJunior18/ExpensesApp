7d7d83c65af8d6054c309b7f7cefb80f063bb5ae
Laboratory 07 <lab07@lab.mu.edu>
2025-11-25 12:09:43 +0200

    done save data

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,4 +1,6 @@
+import 'package:expenses_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -17,7 +19,7 @@ class _NewExpenseState extends State<NewExpense> {
 
   String expenseAmount = '';
   void _saveExpenseAmount(String input) {
-    expenseTitle = input;
+    expenseAmount = input;
   }
 
   final _amountController = TextEditingController();
@@ -28,11 +30,31 @@ class _NewExpenseState extends State<NewExpense> {
     super.dispose();
   }
 
+  var _selectedDate;
+
+  void _showDatePicker() async {
+    var now = DateTime.now();
+    var firstDate = DateTime(now.year - 1, now.month, now.day);
+    var pickedDate = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
+
+  Category _selectedCategory = Category.leisure;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           TextField(
             onChanged: _saveExpenseTitle,
@@ -44,17 +66,66 @@ class _NewExpenseState extends State<NewExpense> {
             maxLines: null,
             decoration: InputDecoration(label: Text("Expense Title")),
           ),
-          TextField(
-            onChanged: _saveExpenseAmount,
-            controller: _amountController,
-            decoration: InputDecoration(
-              label: Text("Amount"),
-              prefixText: "\$ ",
-            ),
-            keyboardType: TextInputType.number,
-          ),
           Row(
             children: [
+              Expanded(
+                child: TextField(
+                  onChanged: _saveExpenseAmount,
+                  controller: _amountController,
+                  decoration: InputDecoration(
+                    label: Text("Amount"),
+                    prefixText: "\$ ",
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+              ),
+              SizedBox(height: 16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      _selectedDate == null
+                          ? 'No Date Selected'
+                          : DateFormat.yMd().format(_selectedDate!),
+                    ),
+                    IconButton(
+                      onPressed: _showDatePicker,
+                      icon: Icon(Icons.calendar_month),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 16),
+          Row(
+            children: [
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(category.name.toUpperCase()),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  if (value == null) {
+                    return;
+                  }
+                  setState(() {
+                    _selectedCategory = value;
+                  });
+                },
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Cancel"),
+              ),
               ElevatedButton(
                 onPressed: () {
                   print(expenseTitle);
